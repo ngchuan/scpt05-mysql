2f75128b0f2a63e8863f4dc27cfc978c880e7978
ngchuan <ngchuan@yahoo.com.sg>
2024-10-28 08:30:12 +0000

    third draft

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { createConnection } = require('mysql2/promise');
 let app = express();
 app.set('view engine', 'hbs');
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
 wax.on(hbs.handlebars);
 wax.setLayoutPath('./views/layouts');
@@ -29,7 +29,7 @@ async function main() {
         'password': process.env.DB_PASSWORD
     })
 
-    app.get('/', (req,res) => {
+    app.get('/', (req, res) => {
         res.send('Hello, World!');
     });
 
@@ -40,28 +40,70 @@ async function main() {
             'customers': customers
         })
     })
-    
- // Add New Customer Step 5.3
-    app.get('/customers/create', async(req,res)=>{
-        let [companies] = await connection.execute('SELECT * from Companies');
-        let [employees] = await connection.execute('SELECT * from Employees');
-        res.render('customers/create', {
-            'companies': companies,
+
+    //show employees list in /view/employees/index.hbs
+    app.get('/employees', async (req, res) => {
+        let [employees] = await connection.execute(`
+            SELECT * 
+            FROM Employees 
+            INNER JOIN Departments 
+            ON Employees.department_id = Departments.department_id
+        `);
+
+        // Format date_joined for each employee
+        employees = employees.map(employee => {
+            let dateJoined = new Date(employee.date_joined); // assuming date_joined is the field name
+            let formattedDate = dateJoined.toISOString().split('T')[0]; // yyyy-mm-dd format
+            return { ...employee, date_joined: formattedDate };
+        });
+
+        res.render('employees/index', {
             'employees': employees
         })
     })
-    
-    app.post('/customers/create', async(req,res)=>{
-        let {first_name, last_name, rating, company_id, employee_id} = req.body;
-        let query = 'INSERT INTO Customers (first_name, last_name, rating, company_id) VALUES (?, ?, ?, ?)';
+
+
+    // Add New Customer Step 5.3
+    app.get('/customers/create', async (req, res) => {
+        let [companies] = await connection.execute('SELECT * from Companies');
+        //    let [employees] = await connection.execute('SELECT * from Employees');
+        res.render('customers/create', {
+            'companies': companies
+            //        'employees': employees
+        })
+    })
+
+    // Add New Employee Step 5.3
+    app.get('/employees/create', async (req, res) => {
+        let [departments] = await connection.execute('SELECT * from Departments');
+        let [employees] = await connection.execute('SELECT * from Employees');
+        res.render('employees/create', {
+            'departments': departments,
+            'employees': employees
+        })
+    })
+
+    app.post('/customers/create', async (req, res) => {
+        let { first_name, last_name, rating, company_id } = req.body;
+        let query = 'INSERT INTO Customers (first_name, last_name, rating, company_id ) VALUES (?, ?, ?, ?)';
         let bindings = [first_name, last_name, rating, company_id];
         let [result] = await connection.execute(query, bindings);
-    
+
         let newCustomerId = result.insertId;
         res.redirect('/customers');
     })
-    
-     // Edit A Specific Customer Step 5.4
+
+    app.post('/employees/create', async (req, res) => {
+        let { first_name, last_name, designation, department_id, contact, date_joined } = req.body;
+        let query = 'INSERT INTO Employees (first_name, last_name, designation, department_id, contact, date_joined) VALUES (?, ?, ?, ?, ?, ?)';
+        let bindings = [first_name, last_name, designation, department_id, contact, date_joined];
+        let [result] = await connection.execute(query, bindings);
+
+        let newEmployeeId = result.insertId;
+        res.redirect('/employees');
+    })
+
+    // Edit A Specific Customer Step 5.4
     app.get('/customers/:customer_id/edit', async (req, res) => {
         let [customers] = await connection.execute('SELECT * from Customers WHERE customer_id = ?', [req.params.customer_id]);
         let [companies] = await connection.execute('SELECT * from Companies');
@@ -71,38 +113,79 @@ async function main() {
             'companies': companies
         })
     })
-    
+
+    // Edit A Specific Employee Step 5.4
+    app.get('/employees/:employee_id/edit', async (req, res) => {
+        let [employees] = await connection.execute('SELECT * from Employees WHERE employee_id = ?', [req.params.employee_id]);
+        let [departments] = await connection.execute('SELECT * from Departments');
+        let employee = employees[0];
+        res.render('employees/edit', {
+            'employee': employee,
+            'departments': departments
+        })
+    })
+
     app.post('/customers/:customer_id/edit', async (req, res) => {
-        let {first_name, last_name, rating, company_id, employee_id} = req.body;
-    
-        let query = 'UPDATE Customers SET first_name=?, last_name=?, rating=?, company_id=? WHERE customer_id=?';
+        let { first_name, last_name, rating, company_id } = req.body;
+
+        let query = 'UPDATE Customers SET first_name=?, last_name=?, rating=?, company_id=?, WHERE customer_id=?';
         let bindings = [first_name, last_name, rating, company_id, req.params.customer_id];
         await connection.execute(query, bindings);
-        
+
         res.redirect('/customers');
     });
-    
-    app.get('/customers/:customer_id/delete', async function(req,res){
+
+    app.post('/employees/:employee_id/edit', async (req, res) => {
+        let { first_name, last_name, designation, department_id, contact, date_joined } = req.body;
+
+        let query = 'UPDATE Employees SET first_name=?, last_name=?, designation=?, department_id=?, contact=?, date_joined=? WHERE employee_id=?';
+        let bindings = [first_name, last_name, designation, department_id, contact, date_joined, req.params.employee_id];
+        await connection.execute(query, bindings);
+
+        res.redirect('/employees');
+    });
+
+    // delete a customer record
+    app.get('/customers/:customer_id/delete', async function (req, res) {
         // display a confirmation form 
         const [customers] = await connection.execute(
-            "SELECT * FROM Customers WHERE customer_id =?", [req.params.customer_id]
+            "SELECT * FROM Customers WHERE customer_id = ?", [req.params.customer_id]
         );
         const customer = customers[0];
 
         res.render('customers/delete', {
             customer
-        })
-
-        app.post('/customers/:customer_id/delete', async function(req, res){
+        });
+    });
+        app.post('/customers/:customer_id/delete', async function (req, res) {
             await connection.execute(`DELETE FROM Customers WHERE customer_id = ?`, [req.params.customer_id]);
             res.redirect('/customers');
-        })
+        });
 
+        // delete a employee record    
+        app.get('/employees/:employee_id/delete', async function (req, res) {
+            // display a confirmation form 
+            const [employees] = await connection.execute(
+                "SELECT * FROM Employees WHERE employee_id =?", [req.params.employee_id]
+    );
+        const employee = employees[0];
+
+        res.render('employees/delete', {
+            employee
+        });
+    });   
+        app.post('/employees/:employee_id/delete', async function (req, res) {
+            await connection.execute(`DELETE FROM Employees WHERE employee_id = ?`, [req.params.employee_id]);
+            res.redirect('/employees');
+        });
+
+
+
+}
+
+    app.listen(3000, () => {
+        console.log('Server is running')
     })
 
-    app.listen(3000, ()=>{
-        console.log('Server is running')
-    });
-}
 
 main();
